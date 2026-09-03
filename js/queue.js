65d922cca189706a8450b86f214279ab41d9e9c1
Nicci <hello@nicciyin.com>
2026-09-03 10:01:19 -0700

    removed year

--- js/queue.js
+++ js/queue.js
@@ -1,13 +1,13 @@
 var words1 = [
-    '1922:', 'She', 'stepped', 'out', 'past', 'the', 'heavy', 'glass', 'doors', 'and', 'walked', 'into', 'a', 'sea', 'of', 'murmurs', 'H', 'H...E', 'He', 'what?', 'The', 'murmuring', 'winds', 'down', 'a', 'ripple', 'of', 'heads', 'crane', 'up', 'H', 'E', 'L', 'L', 'Waves', 'of', 'gasps', 'and', 'prayers', 'chanted', 'under', 'breath', 'replace', 'the', 'silence', 'what', 'makes', 'them', 'so', 'certain', 'God', 'speaks', 'English', 'H', 'E', 'L', 'L', 'O', 'Oh', 'hell', 'It', 'is', 'just', 'a', 'hello'
+    'She', 'stepped', 'out', 'past', 'the', 'heavy', 'glass', 'doors', 'and', 'walked', 'into', 'a', 'sea', 'of', 'murmurs', 'H', 'H...E', 'He', 'what?', 'The', 'murmuring', 'winds', 'down', 'a', 'ripple', 'of', 'heads', 'crane', 'up', 'H', 'E', 'L', 'L', 'Waves', 'of', 'gasps', 'and', 'prayers', 'chanted', 'under', 'breath', 'replace', 'the', 'silence', 'what', 'makes', 'them', 'so', 'certain', 'God', 'speaks', 'English', 'H', 'E', 'L', 'L', 'O', 'Oh', 'hell', 'It', 'is', 'just', 'a', 'hello'
 ]
 
 var words2 = [
-    '2009:', 'The', 'instruction', 'was', 'written', 'on', 'a', 'card', 'by', 'some', 'famous', 'artist', 'and', 'forgotten', 'for', '40', 'years', 'Today', 'they', 'gather', 'in', 'a', 'hipster', 'backyard', 'one', 'hand', 'holding', 'lukewarm', 'white', 'wine', 'served', 'in', 'cheap', 'plastic', 'name', 'dropping', 'this-or-that', 'installation', 'they', 'saw', 'of', 'this', 'famous', 'artist', 'shame', 'they', 'didn’t', 'do', 'it', 'then', 'this', 'land', 'art', 'piece', 'in', 'the', 'air', '—', 'yes', 'yes', 'wasn’t', 'it', 'brilliant'
+    'The', 'instruction', 'was', 'written', 'on', 'a', 'card', 'by', 'some', 'famous', 'artist', 'and', 'forgotten', 'for', '40', 'years', 'Today', 'they', 'gather', 'in', 'a', 'hipster', 'backyard', 'one', 'hand', 'holding', 'lukewarm', 'white', 'wine', 'served', 'in', 'cheap', 'plastic', 'name', 'dropping', 'this-or-that', 'installation', 'they', 'saw', 'of', 'this', 'famous', 'artist', 'shame', 'they', 'didn’t', 'do', 'it', 'then', 'this', 'land', 'art', 'piece', 'in', 'the', 'air', '—', 'yes', 'yes', 'wasn’t', 'it', 'brilliant'
 ]
 
 var words3 = [
-    '2012:', 'Remember', 'if', 'you', 'fly', 'high', 'enough', 'they', 'can’t', 'even', 'see', 'you', 'He', 'lines', 'up', 'in', 'formation', 'with', 'the', 'rest', 'five', 'altogether', 'ready', 'for', 'this', 'choreography', 'they’ve', 'done', 'all', 'too', 'many', 'times', 'Take', 'off', 'space', 'out', 'equal', 'distance', 'between', 'Puff', 'puff', 'gap', 'gap', 'puff', 'gap', 'puff', 'Somewhere', 'on', 'the', 'ground', 'a', 'shriek', 'and', 'a', 'yes', 'Applause', 'beneath', 'silent', 'magic'
+    'Remember', 'if', 'you', 'fly', 'high', 'enough', 'they', 'can’t', 'even', 'see', 'you', 'He', 'lines', 'up', 'in', 'formation', 'with', 'the', 'rest', 'five', 'altogether', 'ready', 'for', 'this', 'choreography', 'they’ve', 'done', 'all', 'too', 'many', 'times', 'Take', 'off', 'space', 'out', 'equal', 'distance', 'between', 'Puff', 'puff', 'gap', 'gap', 'puff', 'gap', 'puff', 'Somewhere', 'on', 'the', 'ground', 'a', 'shriek', 'and', 'a', 'yes', 'Applause', 'beneath', 'silent', 'magic'
 ]
 
 /*var test = [
